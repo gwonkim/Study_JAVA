1f2de7c8a6fcd9c99f5c2b6e392061c68d2812eb
gwonkim <wo41896804@gmail.com>
2020-09-12 15:34:43 +0900

    hw test

--- 2020_ds/hw0_1/src/hw0_1/Hello.java
+++ 2020_ds/hw0_1/src/hw0_1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hw0_1 : ±èÁö¿ø");
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
