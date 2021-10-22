8a329bcf45cc136240994d9d568e5138920d0df6
kim, jiwon <wo41896804@gmail.com>
2021-10-23 00:04:13 +0900

    resultmap_departmentWithstudent
    시험공부 연습

---
+++ 2021_backend/resultmapPractice1/src/main/java/net/skhu/dto/Student.java
@@ -0,0 +1,16 @@
+package net.skhu.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	int id;
+	String studentNumber;
+	String name;
+	int departmentId;
+	int year;
+
+	List<Department> deparment;
+}
